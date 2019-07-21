f59b439fdd9cbc735bf1a6446ed88277aae5856e
Camila Almeida <camila.almeida.br@hotmail.com>
2019-07-20 21:22:45 -0300

    Criar classe Pet

--- ExPetPessoa.py
+++ ExPetPessoa.py
@@ -1,11 +1,22 @@
 class Pessoa:
-    def __init__(self, nome, idade, peso, altura):
-        self.nome = nome
-        self.idade = idade
-        self.peso = peso
-        self.altura = altura
+    def __init__(self, nomeDono, idadeDono):
+        self.nomeDono = nomeDono
+        self.idadeDono = idadeDono
 
-pessoa = Pessoa('Camila', 21, 50, 1.59)
+pessoa = Pessoa('Camila', 21)
 
-print(f"A {pessoa.nome} tem {pessoa.idade} anos.")
 
+class Pet(Pessoa):
+    def __init__(self, nomeDono, idadeDono, nomePet):
+        super().__init__(nomeDono, idadeDono)
+        self.nomePet = nomePet
+
+    def idadePet(self):
+        ano_nasc = int(input(f'Digite o ano que o {self.nomePet} nasceu: '))
+        idade = 2019 - ano_nasc
+        print(f"{self.nomeDono} é dono(a) do {self.nomePet} que possui {idade} anos.")
+
+
+pet = Pet("Camila", 21, "Bidu") 
+pet.idadePet()
+print(f"{pessoa.nomeDono} tem {pessoa.idadeDono} anos.")
